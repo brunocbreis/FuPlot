ab887254e594cb1cb86236fb1c8e72d2e5dfb22d
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-02-10 19:48:25 -0300

    edit values

--- fuplot/test/geom_line_test.py
+++ fuplot/test/geom_line_test.py
@@ -11,11 +11,11 @@ def main():
     # clean dates
     data.Date = pd.to_datetime(data.Date).apply(lambda v: v.value)
 
-    plot = FuPlot(data, aes("Date"), width=0.8, height=0.5)
+    plot = FuPlot(data, aes("Date"), width=0.6, height=0.5)
     plot.geom_line(mapping=aes(y="High"), color=RGBA(0.2, 1, 0.5))
     plot.geom_line(mapping=aes(y="Low"), color=RGBA(1, 0.2, 0.5))
     plot.geom_line(
-        mapping=aes(y="Adj Close"), color=RGBA(0.35, 0.35, 1), thickness=0.0015
+        mapping=aes(y="Adj Close"), color=RGBA(0.35, 0.35, 1), thickness=0.002
     )
 
     plot.render()
