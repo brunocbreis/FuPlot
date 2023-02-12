8abf08a8ae43aa0c116c5b0d32cae3431867282d
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-02-11 22:11:45 -0300

    change test

--- test/geom_point_test.py
+++ test/geom_point_test.py
@@ -4,23 +4,27 @@ from fuplot import FuPlot, RGBA, aes
 
 
 def main() -> None:
-    # data = pd.read_csv(Path("test_data") / "pop_co2.csv")
+    data = pd.read_csv(Path("test_data") / "pop_co2.csv")
 
-    # plot = FuPlot(data, width=0.5)
-    # plot.geom_point(x="pop_urb_pct", y="co2_transport", fill=RGBA(1, 0.2, 0.4, 0.5))
-
-    # pyperclip.copy(plot.render())
-
-    data = pd.read_csv(Path("test_data") / "planets.csv")
-    data = data.dropna()
-
-    plot = FuPlot(data, width=0.6)
+    plot = FuPlot(data, width=0.5)
     plot.geom_point(
-        mapping=aes("distance", "orbital_period", size="mass"),
-        fill=RGBA(0.2, 1, 0.4),
-        opacity=0.1,
+        mapping=aes(x="pop_urb_pct", y="co2_transport"),
+        fill=RGBA(1, 0.2, 0.4),
+        opacity=0.25,
     )
+
     plot.render()
+
+    # data = pd.read_csv(Path("test_data") / "planets.csv")
+    # data = data.dropna()
+
+    # plot = FuPlot(data, width=0.6)
+    # plot.geom_point(
+    #     mapping=aes("distance", "orbital_period", size="mass"),
+    #     fill=RGBA(0.2, 1, 0.4),
+    #     opacity=0.1,
+    # )
+    # plot.render()
 
 
 if __name__ == "__main__":
